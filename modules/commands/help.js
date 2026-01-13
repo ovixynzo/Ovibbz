b4425b7c8eb146214d283e782e30bbfbfd19c336
ovixynzo <ovixynzo@gmail.com>
2026-01-13 15:50:34 +0600

    Update help.js

--- modules/commands/help.js
+++ modules/commands/help.js
@@ -88,8 +88,8 @@ module.exports.run = async function ({ api, event, args }) {
     }
 
     msg += `⭔ Type ${global.config.PREFIX}help [command] to see details\n`;
-    msg += `╭─[⋆˚তোমাগো × লাং🎀⋆˚]\n`;
-    msg += `╰‣ 𝐀𝐝𝐦𝐢𝐧 : Tamim Bbz\n`;
+    msg += `╭─[⋆˚𝗢𝘃𝗶𝗿 𝗯𝗼𝘄⋆˚]\n`;
+    msg += `╰‣ 𝐀𝐝𝐦𝐢𝐧 : Ovi bbz\n`;
     msg += `╰‣ 𝐑𝐢𝐩𝐨𝐫𝐭 : /callad (yourmsg)\n`;
     msg += `╰‣ 𝐓𝐲𝐩𝐞 /help2 𝐭𝐨 𝐬𝐞𝐞 𝐧𝐞𝐱𝐭 𝐩𝐚𝐠𝐞\n`;
     // Attach random GIF for both pages
